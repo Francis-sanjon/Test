a43991b8172a35af28da86b6daacab3f1e419dab
Francis-sanjon <francis.s@annulartechnologies.com>
2022-09-15 17:35:15 +0530

    commit_005

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -4,7 +4,5 @@ describe('empty spec', () => {
   it('Login OrangeHRM', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
   })
-  it('Test', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.')
-  })
+ 
 })
