cd1083dbecdd595d0ec7875a99a58f25fb0d61a3
Francis-sanjon <francis.s@annulartechnologies.com>
2022-09-19 11:23:36 +0530

    commit_008

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,9 +1,12 @@
-const { Given } = require("@badeball/cypress-cucumber-preprocessor")
+const { Given, When } = require("@badeball/cypress-cucumber-preprocessor")
 
 
 describe('empty spec', () => {
   Given('User login OrangeHRM page', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  })
+  When('User enters the valid username and password',()=>{
+    
   })
 
         })
--- cypress/e2e/Login.feature
+++ cypress/e2e/Login.feature
@@ -2,4 +2,5 @@ Feature: Login
 
    Scenario: Login OrangeHRM
    Given User login OrangeHRM page
+   When User enters the valid username and password
    
