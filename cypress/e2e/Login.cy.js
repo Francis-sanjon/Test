85b3eab0c182f9dcba2a1ba039514e292e953e7e
Francis-sanjon <francis.s@annulartechnologies.com>
2022-09-19 11:31:14 +0530

    commit_009

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -6,7 +6,8 @@ describe('empty spec', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
   })
   When('User enters the valid username and password',()=>{
-    
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
   })
 
         })
